1c543d4520ff11b5c9569f1a14b5f0d57a252d36
Saurabh Singh <Saurabh.s.kshatriya7@gmail.com>
2021-07-27 19:56:24 +0530

    added configs

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2020 The Xtended Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/xtended_sweet.mk
+
+COMMON_LUNCH_CHOICES := \
+    xtended_sweet-user \
+    xtended_sweet-userdebug \
+    xtended_sweet-eng
